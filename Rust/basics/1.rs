80c251ffd565ab2ca81db7c8da723fd1ff928561
yousif <138237408+yousifj129@users.noreply.github.com>
2025-01-16 09:47:15 +0300

    Update 1.rs

--- Rust/basics/1.rs
+++ Rust/basics/1.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, Rust World!");
+    let constellations = 88; // We make a new sticky note and write a value on it
+    println!("{}", constellations); // Prints out: 88
+    
+    constellations = 77; // We change the note's details
+    println!("{}", constellations); // Prints out: 77
+    
 }
